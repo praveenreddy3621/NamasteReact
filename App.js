84c3d44d10787c6bf36c81a3c0d24dee22d3ba44
p0m07na <praveenreddy.midiges@walmart.com>
2023-01-02 15:48:40 -0800

    Second Commit

--- App.js
+++ App.js
@@ -1,3 +1,29 @@
+// Why React is fast?
+// React uses Bundeler - (Parcel)
+
+// Parcel - 
+// Created A Server
+// Hot Module Replacement (HMR)
+// File Watcher Algoithm - c++
+// Bundling
+// Minify
+// Cleaning our Code
+// Dev & Production Build
+// Super fast build algorithm
+// Image Optimization
+// Caching while developemnt
+// Compression
+// Compatable with older version of browser
+// HTTPS on dev
+// Port numbers
+// Consistent Hasing Algorithm
+// Zero Config Bundler
+// 
+
+
+import React from "react";
+import ReactDOM from "react-dom/client";
+
 const heading = React.createElement(
     "h1",
     {},
@@ -9,13 +35,15 @@ const heading = React.createElement(
 
    // const root = ReactDOM.createRoot(document.getElementById("root"));
 
-   const header1Element  = React.createElement('h1', {id:'title'}, "Hello I am h1 tag")
+   const header1Element  = React.createElement('h1', {id:'header1', hello: 'World'}, "Hello I am h1 tag")
    // this react element is an Object
    console.log(".......Printing header1Element>>>", header1Element)
-   const header2Element = React.createElement('h2', {id:'title'}, "Hello I am h2 tag")
+   const header2Element = React.createElement('h2', {id:'header'}, 'I am Header 2')
 
    const divElement = React.createElement('div', {id:'container'}, header1Element, header2Element, React.createElement('h3',{id: 'header3'}, 'I am header3'))
 
    const root = ReactDOM.createRoot(document.getElementById('root'));
   // passing react element in to render
   root.render(divElement);
+
+ 
